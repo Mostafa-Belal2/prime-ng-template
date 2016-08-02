70f9b17f199118b5946491ea516daf3e246d3810
cemre.mengu <cemre.mengu@ttgint.com>
2016-08-02 19:26:56 +0300

    Fixed form-control issue

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
 import { HTTP_PROVIDERS } from '@angular/http';
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrap(AppComponent, [
-    HTTP_PROVIDERS
+    HTTP_PROVIDERS,
+    disableDeprecatedForms(),
+    provideForms()
 ]);
